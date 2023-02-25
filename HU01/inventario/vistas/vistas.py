95ea9967813766e4f2af379047e6cda11f6fe14f
Benito Zarate Palomec <b.zarate@uniandes.edu.co>
2023-02-24 20:09:56 -0600

    actualizaciòn

--- HU01/inventario/vistas/vistas.py
+++ HU01/inventario/vistas/vistas.py
@@ -4,7 +4,13 @@ from celery import Celery
 from flask_restful import Resource
 
 inventario_schema = InventarioSchema()
-celery = Celery(__name__, broker='redis://localhost:6379/0')
+
+
+BROKER_URL = 'redis://localhost:6379/0'
+BACKEND_URL = 'redis://localhost:6379/1'
+CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+
+celery = Celery(__name__, broker=BROKER_URL, backend=BACKEND_URL)
 
 
 @celery.task(name="consultar_inventario_producto")
@@ -16,5 +22,7 @@ class VistaTablaInventario(Resource):
     def get(self):
         result = consultar_inventario_producto.apply_async(
             queue='inventario_producto')
-        print(result.get())
+            #  id, nombre, precio
+            # producto_id, existencia
+            # --> id, nombre, precio, existencia
         return result.get()
--- HU01/producto/tareas/tareas.py
+++ HU01/producto/tareas/tareas.py
@@ -1,14 +1,22 @@
+from datetime import datetime
+from sqlite3 import Date
 from celery import Celery
 from celery.signals import task_postrun
 from flask.globals import current_app
 
-app = Celery(__name__, broker='redis://localhost:6379/0')
+
+BROKER_URL = 'redis://localhost:6379/0'
+BACKEND_URL = 'redis://localhost:6379/1'
+CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+
+app = Celery(__name__, broker=BROKER_URL, backend=BACKEND_URL)
+#app = Celery(__name__, broker='redis://localhost:6379/0')
 
 
 @app.task(name="consultar_inventario_producto")
 def consultar_productos():
-    print('as')
-    return 'Respuesta cola de mensaje listado de productos'
+    #consultar de la base de datos la informaciòn de todos los productos y retornar el json: id, nombre, precio
+    return 'Respuesta cola de mensaje listado de productos - ' + str( datetime.now())
 
 @task_postrun.connect
 def close_session(*args, **kwargs):
